5ac6a205f05805ba8c14ae27ec797c763a342070
daniellebandong <46451872+daniellebandong@users.noreply.github.com>
2021-03-30 15:12:22 -0600

    all the logos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import firebaseApp from './firebase/firebaseConfig';
+function App() {
+ console.log(firebaseApp);
+  return (
+   <div>
+     <header>
+       <h1>React Firebase</h1>
+     </header>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAN3R3q4K-QVmIB1eWBi12mkws5iN91boE",
+    authDomain: "employee-manager-sec-1.firebaseapp.com",
+    projectId: "employee-manager-sec-1",
+    storageBucket: "employee-manager-sec-1.appspot.com",
+    messagingSenderId: "213882699381",
+    appId: "1:213882699381:web:7b39890d186e07118a823c",
+    measurementId: "G-J90TGZL467"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
