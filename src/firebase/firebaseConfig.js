bfe986be2f9e1bc181fb96efc856de71a639eb12
daniellebandong <46451872+daniellebandong@users.noreply.github.com>
2021-04-05 20:29:24 -0600

    firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,15 +3,17 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAN3R3q4K-QVmIB1eWBi12mkws5iN91boE",
-    authDomain: "employee-manager-sec-1.firebaseapp.com",
-    projectId: "employee-manager-sec-1",
-    storageBucket: "employee-manager-sec-1.appspot.com",
-    messagingSenderId: "213882699381",
-    appId: "1:213882699381:web:7b39890d186e07118a823c",
-    measurementId: "G-J90TGZL467"
-  };
+  apiKey: "AIzaSyAN3R3q4K-QVmIB1eWBi12mkws5iN91boE",
+  authDomain: "employee-manager-sec-1.firebaseapp.com",
+  databaseURL: "https://employee-manager-sec-1-default-rtdb.firebaseio.com",
+  projectId: "employee-manager-sec-1",
+  storageBucket: "employee-manager-sec-1.appspot.com",
+  messagingSenderId: "213882699381",
+  appId: "1:213882699381:web:e47d9f6234659e8f8a823c",
+  measurementId: "G-8K7WGZRDZW"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
